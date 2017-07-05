15efbe0df7f47e6da7573f587e537547e6db7834
jakubskiba <jakubskibakrk@gmail.com>
2017-07-05 09:38:52 +0200

    Implement real student class (model)

--- models/student_model.py
+++ models/student_model.py
@@ -1,8 +1,26 @@
-class Student():
-    """
-    Mock student class
-    """
-    def __init__(self, name):
-        self.name = name 
+from models.user_model import User
 
-    pass
+
+class Student(User):
+    """
+    Attributes:
+        assignment_submisions (list)
+        grades_submissions (list)
+        attendance_list (list)
+    """
+
+    def __init__(self, assignment_submissions, grades_submissions, attendance_list):
+        if isinstance(assignment_submissions, list):
+            self.assignment_submissions = assignment_submissions
+        else:
+            raise TypeError(assignment_submissions + ' is not list')
+
+        if isinstance(grades_submissions, list):
+            self.grades_submissions = grades_submissions
+        else:
+            raise TypeError(grades_submissions, + ' is not list')
+
+        if isinstance(attendance_list):
+            self.attendance_list = attendance_list
+        else:
+            raise TypeError(attendance_list + ' is not list')
