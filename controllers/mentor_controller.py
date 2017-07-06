daddca6e08aa78ce2cd5ecc826658d5566a30bcb
Maciej Nowak <mcnowak@wp.pl>
2017-07-06 10:31:54 +0200

    Add mentor controller basic functionality

---
+++ controllers/mentor_controller.py
@@ -0,0 +1,24 @@
+from models.mentor_model import Mentor
+from views.mentor_view import *
+
+
+def start_controller(codecool, mentor):
+    choice = ''
+    while choice != '0':
+        print_mentor_menu()
+        choice = get_choice()
+
+        if choice == '1':
+            print_students_list(codecool)
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            pass
+
+
+def submit_assignment():
+    pass
--- views/mentor_view.py
+++ views/mentor_view.py
@@ -1,3 +1,6 @@
+import views.ui
+
+
 def print_mentor_details(mentor):
     """
     Function prints mentor details
@@ -16,3 +19,29 @@ def print_mentor_details(mentor):
     print('E-mail:', mentor.e_mail)
     print('Phone:', mentor.phone)
     print(ornament)
+
+
+def print_students_list(codecool):
+    title_list = ['id', 'Name', 'Surname']
+
+    table = []
+    for student in codecool.students_list:
+        table.append([student.id_, student.name, student.surname])
+
+    views.ui.print_table(table, title_list)
+
+
+def print_mentor_menu():
+    title = 'Mentor Menu'
+    options = ['List students', 'Add assignments', 'Grade assignment', 'Check attendance', 'Edit student']
+
+    views.ui.print_menu(title, options, 'Exit')
+
+
+def get_choice():
+    possible_choices = ['0', '1', '2', '3', '4', '5']
+    choice = ''
+    while choice not in possible_choices:
+        choice = views.ui.get_inputs(['option:'], 'Choose option')[0]
+
+    return choice
