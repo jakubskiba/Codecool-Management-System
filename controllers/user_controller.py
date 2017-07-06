d4a727294d43d8005596991abaf16e2a56f54ad3
wera dabrowska <weronikad752@gmail.com>
2017-07-06 11:24:03 +0200

    menu in user controller

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -3,6 +3,5 @@ from views.user_view import *
 
 
 def main():
-    wera = User('w', 'd', 'wd', 'p', '@', '123', 0)
-    display_update_choice(wera)
-    a = get_input()
+    
+    display_update_choice
--- views/user_view.py
+++ views/user_view.py
@@ -1,24 +1,4 @@
-from ui import print_menu
-
-    """
-    Displays a menu. Sample output:
-        Main menu:
-            (1) Store manager
-            (2) Human resources manager
-            (3) Inventory manager
-            (4) Accounting manager
-            (5) Sales manager
-            (6) Customer relationship management (CRM)
-            (0) Exit program
-
-    Args:
-        title (str): menu title
-        list_options (list): list of strings - options that will be shown in menu
-        exit_message (str): the last option with (0) (example: "Back to main menu")
-
-    Returns:
-        This function doesn't return anything it only prints to console.
-    """
+from views.ui import print_menu
 
 
 def get_input():
@@ -28,5 +8,5 @@ def get_input():
 
 def display_update_choice(user):
 
-    attributes_list = [attr for attr in dir(user) if not attr.startswith('__')]
+    attributes_list = [attr for attr in dir(user) if '_' not in attr]
     print_menu('Which data to update? enter number', attributes_list, 'exit')
