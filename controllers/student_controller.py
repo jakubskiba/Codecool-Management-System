027f31e0b2ac2c1991a9860494e9544a03bfb0d9
jakubskiba <jakubskibakrk@gmail.com>
2017-07-06 11:07:21 +0200

    implement start_controller function in student controller module

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,28 +1,29 @@
-from models.student_model import Student
-from models.school_model import School
-from views.student_view import print_student_menu, get_choice, print_all_submissions, get_submission_number
-# from views.assignment_submission_view import print_submission
+from views.student_view import *
+from models.assignment_submission_model import AssignmentSubmission
+from datetime import datetime
 
 
-def start_controller(student, school):
+def start_controller(school, student):
     choice = ''
     while choice != '0':
         print_student_menu()
         choice = get_choice()
 
         if choice == '1':
-            submit_assignment()
+            submit_assignment(school, student)
         elif choice == '2':
+            list_assignments(school)
+        elif choice == '3':
             get_assignment_submissions(student)
 
 
+def submit_assignment(school, student):
+    pass
+
+
+def list_assignments(school):
+    pass
+
+
 def get_assignment_submissions(student):
-    choice = ''
-    print_all_submissions(student.assignment_submissions) 
-    while choice != '0':
-        print_all_submissions(student.assignment_submissions)
-        submission_number = get_submission_number(student.assignment_submissions)
-
-
-def submit_assignment():
     pass
