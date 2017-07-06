6693246135714ffbd331f9a6bf812ea6177c50e9
jakubskiba <jakubskibakrk@gmail.com>
2017-07-06 13:50:55 +0200

    refactore functions

--- controllers/manager_controller.py
+++ controllers/manager_controller.py
@@ -6,22 +6,22 @@ from models.user_model import User
 def start_controller(school, manager):
     choice = ''
     while choice != '0':
-        print(User.last_id)
         print_manager_menu()
         choice = get_choice()
 
         if choice == '1':
             list_all_mentors(school.mentors_list)
+
         elif choice == '2':
             view_mentor_details(school)
-            pass
-        elif choice == '3':
-            add_mentor(school, manager)
 
-            pass
+        elif choice == '3':
+            add_mentor(school)
+
         elif choice == '4':
             # Remove mentor
             pass
+
         elif choice == '5':
             # List students
             pass
@@ -30,7 +30,7 @@ def start_controller(school, manager):
             pass
 
 
-def view_mentor_details(school):
+def get_mentor(school):
     mentors_ids = [str(mentor.id_) for mentor in school.mentors_list]
 
     chosen_mentor_id = ''
@@ -43,10 +43,15 @@ def view_mentor_details(school):
         if chosen_mentor_id == mentor.id_:
             chosen_mentor = mentor
 
+    return chosen_mentor
+
+
+def view_mentor_details(school):
+    chosen_mentor = get_mentor(school)
     print_mentor(chosen_mentor)
 
 
-def add_mentor(school, manager):
+def add_mentor(school):
     mentor_data = get_new_mentor_data()
 
     name = mentor_data[0]
@@ -61,3 +66,5 @@ def add_mentor(school, manager):
     new_mentor = Mentor(name, surname, login, password, email, phone, id_)
 
     school.mentors_list.append(new_mentor)
+
+
