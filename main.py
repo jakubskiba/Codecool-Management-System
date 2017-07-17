4ea35fb1c0ec3fe8696c2bbee7083d69b03b6231
jakubskiba <jakubskibakrk@gmail.com>
2017-07-17 18:59:24 +0200

    implement error log

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from controllers import school_controller
+import sys
+import os
+import traceback
+import datetime
 
 
 def main():
@@ -6,4 +10,19 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+
+    except BaseException:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+
+        filename = str(datetime.datetime.now())
+        filename = filename.split('.')[0]
+        file_path = 'log/' + filename + '.log'
+
+        log_file = open(file_path, 'w')
+
+        log_file.write('Traceback (most recent call last):\n')
+        traceback.print_tb(exc_traceback, file=log_file)
+        log_file.write(str(exc_type.__name__) + ': ')
+        log_file.write(str(exc_value))
