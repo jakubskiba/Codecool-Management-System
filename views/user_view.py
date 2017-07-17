7ac6a6e1f88416cd4d52ab035d767bfe5d3a9d8f
jakubskiba <jakubskibakrk@gmail.com>
2017-07-17 19:20:54 +0200

    implement display_user_info function

--- views/user_view.py
+++ views/user_view.py
@@ -24,3 +24,16 @@ def display_update_choice(user):
     attributes_list = [attr for attr in dir(user) if '_' not in attr]
     print_menu('Which data to update? enter number', attributes_list, 'exit')
 
+
+def display_user_info(user):
+    """
+    Displays who is logged in
+
+    Args:
+        user(obj)
+
+    Returns:
+        None
+    """
+
+    print('Name: {} Surname: {}\nPermissions: {}'.format(user.name, user.surname, user.__class__.__name__))
