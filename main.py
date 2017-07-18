c433482dee391653e1fe7b7aa22bbd027ea48ec3
jakubskiba <jakubskibakrk@gmail.com>
2017-07-18 09:51:42 +0200

    move handling exception to funtion in utilities modulew

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from controllers import school_controller
-import sys
-import os
-import traceback
-import datetime
-
+import utilities
 
 def main():
     school_controller.start_controller()
@@ -14,15 +10,4 @@ if __name__ == '__main__':
         main()
 
     except BaseException:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-
-        filename = str(datetime.datetime.now())
-        filename = filename.split('.')[0]
-        file_path = 'log/' + filename + '.log'
-
-        log_file = open(file_path, 'w')
-
-        log_file.write('Traceback (most recent call last):\n')
-        traceback.print_tb(exc_traceback, file=log_file)
-        log_file.write(str(exc_type.__name__) + ': ')
-        log_file.write(str(exc_value))
+        utilities.handle_exception()
