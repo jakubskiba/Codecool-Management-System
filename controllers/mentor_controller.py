aebbbdfb6f707d671ceae34c51c13ee27d0623c3
wera dabrowska <weronikad752@gmail.com>
2017-07-18 15:53:42 +0200

    fix import prefixes in mentor_controller

--- controllers/mentor_controller.py
+++ controllers/mentor_controller.py
@@ -1,15 +1,15 @@
 import os
 
-from models.mentor_model import Mentor
-from models.user_model import User
-from models.student_model import Student
+from models import mentor_model  # Mentor : jest szansa że to jest niepotrzebne, ale mózg mi paruje i na wszelkie wypadek nie usunem
+from models import user_model  # User : to tesz 
+from models import student_model  # Student : i to
 
-from views.mentor_view import *
-from views.assignment_view import *
-from views.assignment_submission_view import *
-import views.ui
+from views import mentor_view
+from views import assignment_view
+from views import assignment_submission_view
+from views import ui
 
-from models.assignment_model import Assignment
+from models import assignment_model
 from datetime import datetime
 
 from controllers import user_controller
@@ -30,16 +30,16 @@ def add_new_assignment(codecool):
 
     """
 
-    print_add_assignment_title()
-    content = get_assignment_content()
-    year = get_assignment_year()
-    month = get_assignment_month()
-    day = get_assignment_day()
+    assignment_view.print_add_assignment_title()
+    content = assignment_view.get_assignment_content()
+    year = assignment_view.get_assignment_year()
+    month = assignment_view.get_assignment_month()
+    day = assignment_view.get_assignment_day()
     try:
         date = datetime(int(year), int(month), int(day))
-        assignment = Assignment(content, date, Assignment.last_id + 1)
+        assignment = assignment_model.Assignment(content, date, assignment_model.Assignment.last_id + 1)
         codecool.assignments_list.append(assignment)
-        print_assignment_details(assignment)
+        assignment_view.print_assignment_details(assignment)
     except (TypeError, ValueError) as er:
         print('Wrong values!')
 
@@ -76,7 +76,7 @@ def grade_assignment(codecool):
         None
     """
 
-    print_students_list(codecool)
+    mentor_view.print_students_list(codecool)
     student_id = views.ui.get_inputs([''], 'Choose student id')
 
     try:
@@ -84,7 +84,7 @@ def grade_assignment(codecool):
         id_ = 1
         for subm in student.assignment_submissions:
             print('Submission id:', id_)
-            print_submission(subm)
+            assignment_submission_view.print_submission(subm)
             id_ += 1
         id_ = input('Choose submission id: ')
 
@@ -123,32 +123,6 @@ def choose_submission_by_id(id_, student):
 
 def add_student(codecool):
     user_controller.add_user(codecool, 'student')
-#     """
-#     Append student to students_list in codecool object
-
-#     Args:
-#         codecool (obj): School object - aggregate all users and assignments
-
-#     Returns:
-#         None
-#     """
-
-#     student_data = get_new_student_data()
-
-#     name = student_data[0]
-#     surname = student_data[1]
-#     login = student_data[2]
-#     password = student_data[3]
-#     email = student_data[4]
-#     phone = student_data[5]
-
-#     id_ = User.last_id + 1
-
-#     password = utilities.hash_password(password)
-
-#     new_student = Student(name, surname, login, password, email, phone, id_)
-
-#     codecool.students_list.append(new_student)
 
 
 def get_student(codecool):
@@ -216,11 +190,11 @@ def start_controller(codecool, mentor):
     while choice != '0':
         os.system('clear')
         views.user_view.display_user_info(mentor)
-        print_mentor_menu()
-        choice = get_choice()
+        mentor_view.print_mentor_menu()
+        choice = mentor_view.get_choice()
 
         if choice == '1':
-            print_students_list(codecool)
+            mentor_view.print_students_list(codecool)
         elif choice == '2':
             print_student_details(codecool) 
         elif choice == '3':
