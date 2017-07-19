205a4104ea6cbf7eec2f918a62ab5cf7b4af6176
jakubskiba <jakubskibakrk@gmail.com>
2017-07-19 17:18:11 +0200

    implement show inbox and outbox feature

--- controllers/mail_controller.py
+++ controllers/mail_controller.py
@@ -1,6 +1,10 @@
 from views import mail_view
 from views import user_view
+from controllers.database import get_user_by_id
+from controllers import user_controller
+from models import mail_model
 import os
+from datetime import datetime
 
 
 def start_controller(codecool, user):
@@ -12,20 +16,55 @@ def start_controller(codecool, user):
         choice = mail_view.get_choice()
 
         if choice == '1':
-            inbox = get_inbox(codecool, user)
-            mail_view.print_mail_list(inbox)
+            show_incoming_mail(codecool, user)
 
         elif choice == '2':
-            pass
+            show_outcoming_mail(codecool, user)          
+
+        elif choice == '3':
+            write_email(codecool, user)
 
         if choice != '0':
             input('Press enter')
 
 
-def get_inbox(codecool, user):
-    inbox = []
-    for mail in codecool.mails:
-        if mail.receiver == user:
-            inbox.append(mail)
+def show_incoming_mail(codecool, user):
+    inbox = get_mails(codecool, user, 'in')
+    mail_view.print_mail_list(inbox)
 
-    return inbox
+    idx = mail_view.get_mail_idx(inbox)
+
+    if idx.isdigit() and int(idx) <= len(inbox):
+        os.system('clear')
+        idx = int(idx)
+        mail = inbox[idx]
+        mail_view.print_mail_verbosely(mail)
+
+
+def show_outcoming_mail(codecool, user):
+    outbox = get_mails(codecool, user, 'out')
+    mail_view.print_mail_list(outbox)
+
+    idx = mail_view.get_mail_idx(outbox)
+
+    if idx.isdigit() and int(idx) <= len(outbox):
+        os.system('clear')
+        idx = int(idx)
+        mail = outbox[idx]
+        mail_view.print_mail_verbosely(mail)
+
+
+def get_mails(codecool, user, category):
+    mails = []
+    for mail in codecool.mails:
+
+        if category == 'in' and mail.receiver == user:
+            mails.append(mail)
+        elif category == 'out' and mail.sender == user:
+            mails.append(mail)
+
+    return mails
+
+
+def write_email(codecool, user):
+    pass
--- views/mail_view.py
+++ views/mail_view.py
@@ -1,4 +1,5 @@
 from views import ui
+from math import ceil
 
 
 def print_mail_menu():
@@ -8,7 +9,7 @@ def print_mail_menu():
 
 
 def get_choice():
-    possible_choices = ['0', '1', '2']
+    possible_choices = ['0', '1', '2', '3']
     choice = ''
     while choice not in possible_choices:
         choice = ui.get_inputs(['option:'], 'Choose option')[0]
@@ -17,9 +18,46 @@ def get_choice():
 
 
 def print_mail_list(mail_list):
-    headers = ['From', 'Date', 'Title']
+    headers = ['idx', 'From', 'To', 'Date', 'Title']
     table = []
-    for mail in mail_list:
-        full_name = mail.sender.name + ' ' + mail.sender.surname
-        table.append([full_name, str(mail.date), str(mail.topic)])
+    for i in range(len(mail_list)):
+        mail = mail_list[i]
+        sender_full_name = mail.sender.name + ' ' + mail.sender.surname
+        receiver_full_name = mail.receiver.name + ' ' + mail.receiver.surname
+        table.append([i, sender_full_name, receiver_full_name, str(mail.date), str(mail.topic)])
+
     ui.print_table(table, headers)
+
+
+def get_mail_idx(mail_list):
+    idx = ui.get_inputs(['idx'], 'Provide idx')
+    return idx[0]
+
+
+def print_mail_verbosely(mail):
+    SCREEN_WIDTH = 82
+
+    headers = ['From', 'To', 'Date']
+    headers = ['{:<24}'.format(header) for header in headers]
+    sender_full_name = mail.sender.name + ' ' + mail.sender.surname
+    receiver_full_name = mail.receiver.name + ' ' + mail.receiver.surname
+
+    table = []
+    table.append([sender_full_name, receiver_full_name, str(mail.date)])
+    ui.print_table(table, headers)
+
+    message = []
+    lines_number = ceil(len(mail.message) / SCREEN_WIDTH)
+    for i in range(lines_number):
+        line_begin = i * SCREEN_WIDTH
+        line_end = (i + 1) * SCREEN_WIDTH
+
+        line = mail.message[line_begin:line_end]
+
+        message.append([line])
+
+    headers = ['Title: ' + mail.topic]
+    table = []
+    table.append(message)
+    ui.print_table(message, headers)
+
