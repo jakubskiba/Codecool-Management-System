c2b344c0b12d8824738f64161da7e687b5cda9ee
jakubskiba <jakubskibakrk@gmail.com>
2017-07-19 11:37:16 +0200

    implement mail model

---
+++ models/mail_model.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+class Mail():
+    def __init__(self, date, sender, receiver, topic, message):
+        self.sender = sender
+        self.receiver = receiver
+        self.topic = topic
+        self.message = message
+        self.date = date
