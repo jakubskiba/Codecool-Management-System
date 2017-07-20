e0313cb5cb3a602d44ef6f527e2d4afdfa45305f
Maciej Nowak <mcnowak@wp.pl>
2017-07-20 10:51:09 +0200

    Add mail functionality to manager and mentor controllers

--- views/manager_view.py
+++ views/manager_view.py
@@ -11,14 +11,15 @@ def print_manager_menu():
                'Remove mentor',
                'Edit mentor',
                'List students',
-               'View students details'
+               'View students details',
+               'Mail'
                ]
 
     ui.print_menu(title, options, 'Exit')
 
 
 def get_choice():
-    possible_choices = ['0', '1', '2', '3', '4', '5', '6', '7']
+    possible_choices = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     choice = ''
     while choice not in possible_choices:
         choice = ui.get_inputs(['option:'], 'Choose option')[0]
